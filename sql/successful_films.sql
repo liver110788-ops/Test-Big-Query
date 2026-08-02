05fcce2833575f8d90659b1814fd25241f5277e3
Anton Khyzhnyak <liver110788@gmail.com>
2026-08-02 13:49:50 +0200

    Update successful_films.sql

--- sql/successful_films.sql
+++ sql/successful_films.sql
@@ -1,1 +1,8 @@
-
+SELECT
+    original_title,
+    budget,
+    revenue,
+    revenue - budget AS difference
+FROM `test-data-analyst-123456.the_movie_database.movies`
+WHERE budget < revenue
+  AND revenue > 0
