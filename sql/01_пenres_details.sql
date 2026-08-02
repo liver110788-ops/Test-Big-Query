9d6d7f4cad197ec5cea2ff6d684a9634a45edd1f
Anton Khyzhnyak <liver110788@gmail.com>
2026-08-02 13:41:32 +0200

    Update 01_genres_details.sql

--- sql/01_пenres_details.sql
+++ sql/01_пenres_details.sql
@@ -1,1 +1,39 @@
+WITH GenreCounts AS (
 
+SELECT
+    genre_1,
+    genre_2,
+    COUNT(original_title) AS title_count
+
+FROM `test-data-analyst-123456.the_movie_database.movies`
+
+GROUP BY
+    genre_1,
+    genre_2
+
+),
+
+RankedGenres AS (
+
+SELECT
+    genre_1,
+    genre_2,
+    title_count,
+
+    ROW_NUMBER() OVER(
+        PARTITION BY genre_1
+        ORDER BY title_count DESC
+    ) AS rwnb
+
+FROM GenreCounts
+
+)
+
+SELECT
+    genre_1,
+    genre_2,
+    title_count
+
+FROM RankedGenres
+
+WHERE rwnb <= 3
